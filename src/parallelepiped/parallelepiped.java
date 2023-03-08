237cf2d5b53e5a7666227f626ede5b0ab29c90b7
Liubov Dundieva <lithiumluba@gmail.com>
2023-03-08 21:16:13 +0200

    Homework parallelepiped done

---
+++ src/parallelepiped/parallelepiped.java
@@ -0,0 +1,13 @@
+package parallelepiped;
+
+public class parallelepiped {
+    public static void main(String[] args) {
+        int height = 8;
+        int length = 12;
+        int width = 10;
+        int volume = height*length*width;
+        int sumOfSides = 4*(height+length+width);
+        System.out.println("Об'єм паралелепіпеда = " + volume);
+        System.out.println("Сумарна довжина всіх сторін = " + sumOfSides);
+    }
+}
