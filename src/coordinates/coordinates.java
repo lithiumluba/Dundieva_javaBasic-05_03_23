d2f91d6dc7134970595d564bc92e77d4e409fb03
Liubov Dundieva <lithiumluba@gmail.com>
2023-03-08 21:55:44 +0200

    Homework coordinates done

---
+++ src/coordinates/coordinates.java
@@ -0,0 +1,12 @@
+package coordinates;
+
+public class coordinates {
+    public static void main(String[] args) {
+        double lat = 46.4851465;
+        double lon = 30.6121373;
+        System.out.println("Визначна пам'ятка Одеси - Ахіологійний музей");
+        System.out.println("latitude: " + lat);
+        System.out.println("longitude: " + lon);
+
+    }
+}
