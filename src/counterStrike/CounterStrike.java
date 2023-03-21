cafef032fb760cdc56c70cb780d38cde27174457
Liubov Dundieva <lithiumluba@gmail.com>
2023-03-21 20:22:57 +0200

    Homework CounterStrike done

---
+++ src/counterStrike/CounterStrike.java
@@ -0,0 +1,47 @@
+package counterStrike;
+
+import java.util.Scanner;
+
+public class CounterStrike {
+    public static void main(String[] args) {
+        Scanner inputVal = new Scanner(System.in);
+        String teamA;
+        String teamB;
+        int[] teamAFrags = new int[5];
+        int[] teamBFrags = new int[5];
+        System.out.println("Введіть назву першої команди");
+        teamA = inputVal.nextLine();
+        for (int i = 0; i < teamAFrags.length; i++) {
+            System.out.println("Введить кількість фрагів гравця " + (i + 1));
+            teamAFrags[i] = inputVal.nextInt();
+            inputVal.nextLine();
+        }
+        System.out.println("Введіть назву другої команди");
+        teamB = inputVal.nextLine();
+        for (int i = 0; i < teamBFrags.length; i++) {
+            System.out.println("Введить кількість фрагів гравця " + (i + 1));
+            teamBFrags[i] = inputVal.nextInt();
+            inputVal.nextLine();
+        }
+        int teamASum = 0;
+        int teamAMid;
+        int teamBSum = 0;
+        int teamBMid;
+        for (int teamAFrag : teamAFrags) {
+            teamASum += teamAFrag;
+        }
+        teamAMid = teamASum / teamAFrags.length;
+        for (int teamBFrag : teamBFrags) {
+            teamBSum += teamBFrag;
+        }
+        teamBMid = teamBSum / teamBFrags.length;
+        if (teamASum > teamBSum) {
+            System.out.println("Виграла команда " + teamA + " яка набрала " + teamASum + " балів. Середній бал команди " + teamAMid);
+        } else if (teamASum < teamBSum) {
+            System.out.println("Виграла команда " + teamB + " яка набрала " + teamBSum + " балів. Середній бал команди " + teamBMid);
+        } else {
+            System.out.println("Нічія. Середній бал команди " + teamA + ": " + teamAMid + ". Середній бал команди " + teamB + ": " + teamBMid);
+        }
+
+    }
+}
