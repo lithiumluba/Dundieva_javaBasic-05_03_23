f95c04ea818301fe60d336a4dc386db9f99fe156
Liubov Dundieva <lithiumluba@gmail.com>
2023-03-28 19:42:09 +0300

    Homework Matrix Transposition is done

---
+++ src/matrixTransposition/MatrixTransposition.java
@@ -0,0 +1,41 @@
+package matrixTransposition;
+
+import java.util.Random;
+import java.util.Scanner;
+//Транспонуйте матрицю M x N. Розміри M та N задаються з консолі.
+// Після введення повинні автоматично створюватися два масиви розміром
+// M x N перший та N x M другий.
+
+public class MatrixTransposition {
+    public static void main(String[] args) {
+        Scanner scanIn = new Scanner(System.in);
+        int userInM;
+        int userInN;
+        Random random = new Random();
+
+        System.out.println("Введіть параметр М");
+        userInM = scanIn.nextInt();
+        System.out.println("Введіть параметр N");
+        userInN = scanIn.nextInt();
+        int[][] matrixMN = new int[userInM][userInN];
+        int[][] transMatrix = new int[userInN][userInM];
+
+        System.out.println("Матриця МхN виглядає так:");
+        for (int i = 0; i < userInM; i++) {
+            for (int j = 0; j < userInN; j++) {
+                matrixMN[i][j] = random.nextInt(10);
+                System.out.print(" " + matrixMN[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("Транспанована матриця виглядає так:");
+        for (int i = 0; i < userInN; i++) {
+            for (int j = 0; j < userInM; j++) {
+                transMatrix[i][j] = matrixMN[j][i];
+                System.out.print(" " + transMatrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
