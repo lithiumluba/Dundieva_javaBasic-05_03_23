f476387fc71b86f204dc1f6599fa5e40eb10189a
Liubov Dundieva <lithiumluba@gmail.com>
2023-03-29 19:38:41 +0300

    Homework Guess The Number fix

--- src/guessTheNumber/GuessTheNumber.java
+++ src/guessTheNumber/GuessTheNumber.java
@@ -6,13 +6,13 @@ import java.util.Scanner;
 public class GuessTheNumber {
     public static void main(String[] args) {
         Random rand = new Random();
-        int computer = rand.nextInt(3);
-        Scanner scan_in = new Scanner(System.in);
-        int user_guess;
+        int computer = rand.nextInt(11);
+        Scanner scanIn = new Scanner(System.in);
+        int userGuess;
         for (int i = 3; i > 0; i--) {
             System.out.println("Введіть число від 0 до 10");
-            user_guess = scan_in.nextInt();
-            if (computer == user_guess) {
+            userGuess = scanIn.nextInt();
+            if (computer == userGuess) {
                 System.out.println("Ти вгадав! Це число: " + computer);
                 break;
             } else {
