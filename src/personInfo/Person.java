11f40ee7274b4f19fc0b593971725e8fda4d2cc9
Liubov Dundieva <lithiumluba@gmail.com>
2023-04-10 23:14:50 +0300

    added personInfo homework

--- src/personInfo/Person.java
+++ src/personInfo/Person.java
@@ -1,12 +1,12 @@
 package personInfo;
 
 public class Person {
-    public static String personInfo (String firstName, String lastName, String cityName, String phoneNumber) {
-        return "Зателефонувати громадянину " + firstName +" "+ lastName + " з міста " + cityName + ", можна за номером " + phoneNumber;
+    public static String personInfo(String firstName, String lastName, String cityName, String phoneNumber) {
+        return "Зателефонувати громадянину " + firstName + " " + lastName + " з міста " + cityName + ", можна за номером " + phoneNumber;
     }
     public static void main(String[] args) {
-        System.out.println(personInfo("Will","Smith","New York", "2936729462846"));
-        System.out.println(personInfo("Jackie", "Chan","Shanghai","12312412412"));
-        System.out.println(personInfo("Sherlock","Holmes","London","37742123513"));
+        System.out.println(personInfo("Will", "Smith", "New York", "2936729462846"));
+        System.out.println(personInfo("Jackie", "Chan", "Shanghai", "12312412412"));
+        System.out.println(personInfo("Sherlock", "Holmes", "London", "37742123513"));
     }
 }
